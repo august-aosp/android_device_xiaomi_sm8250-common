68054f1d64684629b0c4feee57b49780ece52042
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-03-24 16:38:02 +0000

    sm8250-common: Disable some QCOM applications
    - These rely on the boost framework which is absent in AOSP ROMs.
    
    Change-Id: I6d60954b9926b1a9ae38c11de5dd0c553b1b6f87
    
    Co-authored-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Change-Id: Ib5afb0e9a6f432db4b7f550de554de4673aa29ac
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- common.mk
+++ common.mk
@@ -318,6 +318,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    DisableQTIAppsCommon \
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
