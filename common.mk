07ac80aacdaa1657b5f0d311e692aa0ee31f4aee
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 08:55:04 +0300

    sm8250-common: Drop soundfx targets that are built by default
    Change-Id: I505758cf06b4992ae8edd5904b9c0b522e48303a

--- common.mk
+++ common.mk
@@ -122,24 +122,16 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     liba2dpoffload \
-    libaudiopreprocessing \
     libbatterylistener \
-    libbundlewrapper \
     libcomprcapture \
-    libdownmix \
-    libdynproc \
-    libeffectproxy \
     libexthwplugin \
     libhdmiedid \
     libhfp \
-    libldnhncr \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libreverbwrapper \
     libsndmonitor \
     libspkrprot \
-    libvisualizer \
     libvolumelistener
 
 # Audio Daemon
