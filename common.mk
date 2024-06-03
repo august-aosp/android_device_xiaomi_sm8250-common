b590116466ca2ee88ef103e4e4a4132ffa106526
EmanuelCN <emanuelghub@gmail.com>
2024-06-03 20:19:00 +0000

    sm8250-common: Override cgroups.json & task_profiles.json * In order to allow uclamp kernels to place tasks correctly

--- common.mk
+++ common.mk
@@ -192,6 +192,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.example
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
