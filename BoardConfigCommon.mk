2de4ad9ad9c264205a10d8616bda752e319dd282
EndCredits <endcredits@crepuscular-aosp.icu>
2024-09-26 21:36:08 +0800

    sm8250-common: Inherit from common xiaomi FCM
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -261,7 +261,8 @@ endif
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     vendor/derp/config/device_framework_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
