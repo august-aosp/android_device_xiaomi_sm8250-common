80bfb581ffc0467f951ed26c55062576cc804241
luffitys <banackidaniel@gmail.com>
2024-11-02 10:47:14 +0100

    sm8250-common: Switch to 64-bit WiFi-Display system daemon
    Change-Id: I6c6b157352734f1cb42d241dd3609f55dcf6616f
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -63,7 +63,7 @@ function blob_fixup() {
         vendor/etc/media_codecs_kona.xml)
             sed -i "/media_codecs_dolby_audio.xml/d" "${2}"
             ;;
-        system_ext/lib/libwfdservice.so)
+        system_ext/lib64/libwfdservice.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
             ;;
