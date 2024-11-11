0eb1504d6df2a5e5694cfee86f847943d1b7679d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-11-11 22:07:39 +0100

    sm8250-common: Import QTI Codec2 service and configs
    From munch
    
    Change-Id: I17a56c596f15a1d5c23582c5c3a7028d3e676aab
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -265,6 +265,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Media configs
+PRODUCT_PACKAGES += \
+    media_codecs_c2.xml \
+    media_codecs_performance_c2.xml
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
