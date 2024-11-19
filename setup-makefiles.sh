739c87302f460ad7fd2a6b357fe143f55a88c21f
EndCredits <endcredits@crepuscular-aosp.icu>
2024-11-19 12:38:30 +0800

    sm8250-common: Support ELF check for libgrallocutils
     * Requires:
      [1]: https://review.lineageos.org/c/LineageOS/android_hardware_qcom_display/+/408811
      [2]: https://review.lineageos.org/c/LineageOS/android_hardware_qcom_display/+/408857
    
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -55,7 +55,6 @@ function lib_to_package_fixup_vendor_variants() {
             echo "${1}_vendor"
             ;;
         libOmxCore | \
-            libgrallocutils | \
             libwpa_client) ;;
         *)
             return 1
