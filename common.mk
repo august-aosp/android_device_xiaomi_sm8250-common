8e00d43ba5cdbb35f389aac81d50894509ed64b0
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    sm8250-common: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- common.mk
+++ common.mk
@@ -472,9 +472,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 endif
 
-# Wi-Fi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
