2c6c67e5b2ed69d1894719429dda9ea365e47dee
hoang1007 <hoangvuhuy1007@gmail.com>
2025-03-17 02:12:47 +0700

    Revert "sm8250-common: Inherit several Android Go configurations"
    This reverts commit fb19f825a3420f2355d5bea2d7f890891ed02522.

--- common.mk
+++ common.mk
@@ -273,10 +273,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
