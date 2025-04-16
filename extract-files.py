013e07a93589312f5bc3da0c9fbbbf81ceb9c1e8
hoang1007 <hoangvuhuy1007@gmail.com>
2025-04-17 01:31:03 +0700

    sm8250-common: Inherit common QTI Adreno component
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -28,6 +28,7 @@ namespace_imports = [
     "vendor/qcom/opensource/commonsys-intf/display",
     "vendor/qcom/opensource/dataservices",
     "vendor/qcom/opensource/display",
+    "vendor/qcom/common/vendor/adreno-r",
 ]
 
 
