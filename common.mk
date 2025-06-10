59a73839e452ea9fd8add334ad0f4f26893bdd9c
Bruno Martins <bgcngm@gmail.com>
2025-06-10 18:44:42 +0200

    sm8250-common: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- common.mk
+++ common.mk
@@ -231,6 +231,9 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_kona)
+
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
