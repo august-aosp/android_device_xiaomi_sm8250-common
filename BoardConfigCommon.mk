9210df9ddd8af79f5f7fd26eb04c5f411e85b519
hoang1007 <hoangvuhuy1007@gmail.com>
2025-10-08 20:57:17 +0700

    sm8250-common: Welcome Lunaris!
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -245,7 +245,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/manifest/framework_compatibility_matrix.xml \
-    vendor/pixelage/config/device_framework_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
