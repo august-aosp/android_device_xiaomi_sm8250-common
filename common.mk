98e74177d887b634d50f568fab9e313f16769010
hoang1007 <hoangvuhuy1007@gmail.com>
2025-12-07 00:32:43 +0700

    sm8250-common: Ship cgroups and task_profiles from device tree
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- common.mk
+++ common.mk
@@ -169,8 +169,8 @@ PRODUCT_PACKAGES += \
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
