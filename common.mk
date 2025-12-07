d3678a6016510c5b65546726ca9a1b57d36f4ed2
hoang1007 <hoangvuhuy1007@gmail.com>
2025-12-07 12:30:29 +0700

    Revert "sm8250-common: Build android.hardware.audio@5.0 as fallback"
    This reverts commit bce582ebebbde77e1f13a1d1b85b1a0d7cb3fb44.

--- common.mk
+++ common.mk
@@ -93,7 +93,6 @@ endif
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
     android.hardware.audio@7.0-impl \
     android.hardware.audio@7.0-util \
     android.hardware.audio.effect@7.0-impl \
